d7b7b0d8b5cd3fdbfa92809f343bfd7d35299a9a
Nataliia Breslavets <breslavets_n@ukr.net>
2023-05-05 22:27:07 +0300

    01

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,5 +1,25 @@
-
+const startBtn = document.querySelector('button[data-start]');
+const stopBtn = document.querySelector('button[data-stop]');
+let timerId = null;
 
 function getRandomHexColor() {
-    return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
-  }
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, '0')}`;
+}
+
+function startColorChange() {
+  startBtn.disabled = true;
+  timerId = setInterval(() => {
+    const color = getRandomHexColor();
+    document.body.style.backgroundColor = color;
+  }, 1000);
+}
+
+function stopColorChange() {
+  startBtn.disabled = false;
+  clearInterval(timerId);
+}
+
+startBtn.addEventListener('click', startColorChange);
+stopBtn.addEventListener('click', stopColorChange);
