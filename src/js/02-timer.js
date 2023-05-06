743a47ee6a619d68090ffe31dff0f7d665951dbc
Nataliia Breslavets <breslavets_n@ukr.net>
2023-05-06 12:55:09 +0300

    Update 02-timer.js

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -82,6 +82,4 @@ function updateTimer({ days, hours, minutes, seconds }) {
 const timePicker = flatpickr('#datetime-picker', options);
 
 btnStart.addEventListener('click', onTimerStart);
-console.log(convertMs(2000)); // {days: 0, hours: 0, minutes: 0, seconds: 2}
-console.log(convertMs(140000)); // {days: 0, hours: 0, minutes: 2, seconds: 20}
-console.log(convertMs(24140000)); // {days: 0, hours: 6 minutes: 42, seconds: 20}
+
